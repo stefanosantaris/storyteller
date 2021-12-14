db96d34f72db49ca227bef13cfafd352ec59b365
Wille Eriksson <wille.eriksson96@gmail.com>
2021-12-14 17:15:31 +0100

    Migrate intent examples from nlu.kt to .exm files

--- StoryTellerSDK/src/main/kotlin/furhatos/app/storyteller/nlu/nlu.kt
+++ StoryTellerSDK/src/main/kotlin/furhatos/app/storyteller/nlu/nlu.kt
@@ -1,47 +1,12 @@
 package furhatos.app.storyteller.nlu
 
 import furhatos.nlu.Intent
-import furhatos.util.Language
 
-class GoToTownSquare: Intent() {
-    override fun getExamples(lang: Language): List<String> {
-        return listOf(
-                "Town square",
-                "I want to go to the town square",
-                "Go to town square",
-                "Go to the square",
-                "I want to go to the square",
-                "Enter town square",
-                "Enter square"
-        )
-    }
-}
+class GoToTownSquare: Intent()
 
-class EnterTavern: Intent() {
-    override fun getExamples(lang: Language): List<String> {
-        return listOf(
-                "Tavern",
-                "I want to go to the tavern",
-                "Go in to the tavern",
-                "Go to the tavern",
-                "I want to go to the tavern",
-                "Enter tavern",
-                "Enter bar",
-                "Enter the door"
-        )
-    }
-}
+class EnterTavern: Intent()
 
-class TalkToWoman: Intent() {
-    override fun getExamples(lang: Language): List<String> {
-        return listOf(
-                "Talk to the woman",
-                "Approach the woman",
-                "Have a chat with the woman",
-                "Ask woman"
-        )
-    }
-}
+class TalkToWoman: Intent()
 
 class TalkToMerchant: Intent()
 
