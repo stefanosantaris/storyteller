cb36acbdda5180b0718f1041643ba2c0e11cade7
Philipp <philipp.mondorf@googlemail.com>
2021-12-17 14:23:16 +0100

    Enable remembering visited scenes

--- StoryTellerSDK/src/main/kotlin/furhatos/app/storyteller/users.kt
+++ StoryTellerSDK/src/main/kotlin/furhatos/app/storyteller/users.kt
@@ -6,3 +6,13 @@ import furhatos.records.User
 var User.name : String? by UserDataDelegate()
 var User.hasPlayed : Boolean? by UserDataDelegate()
 var User.wantsPlay : Boolean? by UserDataDelegate()
+
+/*
+Scene memory
+ */
+var User.visitedTavern : Boolean? by UserDataDelegate()
+var User.visitedTownSquare : Boolean? by UserDataDelegate()
+var User.visitedWoman : Boolean? by UserDataDelegate()
+
+
+
