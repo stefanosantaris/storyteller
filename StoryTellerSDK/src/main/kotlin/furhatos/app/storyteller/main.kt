10ec8565b7d05bc9294418a66360949b448bf5dd
Stefanos Antaris <stefanos.antaris@hivestreaming.com>
2021-12-20 11:16:08 +0100

    Emotion Detection code added

--- StoryTellerSDK/src/main/kotlin/furhatos/app/storyteller/main.kt
+++ StoryTellerSDK/src/main/kotlin/furhatos/app/storyteller/main.kt
@@ -4,8 +4,21 @@ import furhatos.app.storyteller.flow.*
 import furhatos.skills.Skill
 import furhatos.flow.kotlin.*
 
+val objserv = "tcp://localhost:3333"
+val socket: ZMQ.Socket = getConnectedSocket(ZMQ.REQ, objserv)
+
+fun start_emotion_detector() {
+    while (true) {
+        socket.send("Emotion")
+        val message = socket.recvStr()
+        println(message)
+        TimeUnit.SECONDS.sleep(1L)
+    }
+}
+
 class StorytellerSkill : Skill() {
     override fun start() {
+        start_emotion_detector()
         Flow().run(Idle)
     }
 }
