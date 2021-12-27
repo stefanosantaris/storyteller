235c6ca2e6a01bea9e3c9d9442cd524c7592ac2f
Philipp <philipp.mondorf@googlemail.com>
2021-12-27 13:13:05 +0100

    nlu

--- StoryTellerSDK/src/main/kotlin/furhatos/app/storyteller/nlu/BasementIntends.kt
+++ StoryTellerSDK/src/main/kotlin/furhatos/app/storyteller/nlu/BasementIntends.kt
@@ -5,37 +5,171 @@ import furhatos.nlu.Intent
 import furhatos.util.Language
 
 /*
-Express that you want to take the left tunnel
+Express that you want to enter the corridor
  */
-class TalkingMen : EnumEntity(stemming = false, speechRecPhrases = true) {
+class Corridor : EnumEntity(stemming = false, speechRecPhrases = true) {
     override fun getEnum(lang: Language): List<String> {
         return listOf(
-            "Man",
-            "man",
-            "Men",
-            "men",
-            "Talking man",
-            "talking man",
-            "Talking men",
-            "talking men",
-            "Man talking",
-            "man talking",
-            "Men talking",
-            "men talking",
-            "Man whispering",
-            "man whispering",
-            "Men whispering",
-            "men whispering",
-            "Man at the table",
-            "man at the table",
-            "Men at the table",
-            "men at the table"
+            "Corridor",
+            "corridor",
+            "Tunnel",
+            "tunnel",
+            "Gang",
+            "gang",
+            "Gangway",
+            "gangway",
+            "Hallway",
+            "hallway",
+            "Channel",
+            "channel",
+            "Dark",
+            "dark"
         )
     }
 }
 
-class TalkToWhisperingMen(val talkingMen : TalkingMen? = null, val wantTo : WantTo? = null): Intent() {
+class EnterCorridor(val corridor : Corridor? = null, val wantTo: WantTo? = null): Intent() {
     override fun getExamples(lang: Language): List<String> {
-        return listOf("@wantTo @talkingMen", "@wantTo the @talkingMen", "@wantTo a @talkingMen", "@talkingMen", "@wantTo two @talkingMen", "two @talkingMen")
+        return listOf("Enter the @corridor", "enter the @corridor", "Walk into the @corridor", "walk into the @corridor", "walk into @corridor",
+        "into the @corridor", "into @corridor", "@corridor", "go into the @corridor", "go into @corridor", "dark @corridor",
+        "go ahead", "enter", "enter it", "yes", "@wantTo @corridor")}
+}
+
+/*
+Express that you want to leave
+ */
+class Upstairs : EnumEntity(stemming = false, speechRecPhrases = true) {
+    override fun getEnum(lang: Language): List<String> {
+        return listOf(
+            "Tavern",
+            "tavern",
+            "Bar",
+            "bar",
+            "Upstairs",
+            "upstairs",
+            "Up",
+            "up",
+            "Back",
+            "back",
+            "Back up",
+            "back up"
+        )
     }
 }
+
+class GoBack(val upstairs : Upstairs? = null, val wantTo: WantTo? = null): Intent() {
+    override fun getExamples(lang: Language): List<String> {
+        return listOf("go @upstairs", "leave to @upstairs", "@wantTo @upstairs", "@wantTo follow the barmen", "@wantTo leave",
+        "follow the barmen")}
+}
+
+/*
+Express that you would like to enter a corridor
+ */
+class Enter : EnumEntity(stemming = false, speechRecPhrases = true) {
+    override fun getEnum(lang: Language): List<String> {
+        return listOf(
+            "I would like to go to",
+            "I'd like to go to",
+            "I would like to enter",
+            "I'd like to enter",
+            "I want to go to",
+            "I want to enter",
+            "Enter",
+            "enter",
+            "Go to",
+            "go to",
+            "Take the",
+            "take the",
+            "Take",
+            "take",
+            "Walk to",
+            "walk to",
+            "Walk into",
+            "walk into",
+            "Go",
+            "go",
+            "Walk",
+            "walk"
+        )
+    }
+}
+
+class Left : EnumEntity(stemming = false, speechRecPhrases = true) {
+    override fun getEnum(lang: Language): List<String> {
+        return listOf(
+            "Left",
+            "left",
+            "West",
+            "west",
+            "Where west is written",
+            "where west is written",
+            "To my left",
+            "to my left",
+            "To the one on the left",
+            "to the one on the left",
+            "The left one",
+            "the left one"
+        )
+    }
+}
+
+class Straight : EnumEntity(stemming = false, speechRecPhrases = true) {
+    override fun getEnum(lang: Language): List<String> {
+        return listOf(
+            "Straight ahead",
+            "straight ahead",
+            "North",
+            "north",
+            "Where north is written",
+            "where north is written",
+            "Ahead",
+            "ahead",
+            "In front of me",
+            "in front of me",
+            "Straight",
+            "straight"
+        )
+    }
+}
+
+class Right : EnumEntity(stemming = false, speechRecPhrases = true) {
+    override fun getEnum(lang: Language): List<String> {
+        return listOf(
+            "Right",
+            "right",
+            "East",
+            "east",
+            "Where east is written",
+            "where east is written",
+            "To my right",
+            "to my right",
+            "To the one on the right",
+            "to the one on the right",
+            "The right one",
+            "the right one"
+        )
+    }
+}
+
+class GoLeft(val left : Left? = null, val enter: Enter? = null, val corridor : Corridor? = null): Intent() {
+    override fun getExamples(lang: Language): List<String> {
+        return listOf("@enter @left @corridor", "@enter the @left @corridor", "@enter @corridor @left",
+            "@enter the @corridor @left", "@enter @left", "@enter the @left", "@left", "@left @corridor",
+            "@corridor @left", "the @left @corridor")}
+}
+
+class GoStraight(val straight : Straight? = null, val enter: Enter? = null, val corridor : Corridor? = null): Intent() {
+    override fun getExamples(lang: Language): List<String> {
+        return listOf("@enter @straight @corridor", "@enter the @straight @corridor", "@enter @corridor @straight",
+            "@enter the @corridor @straight", "@enter @straight", "@enter the @straight", "@straight", "@straight @corridor",
+            "@corridor @straight", "the @straight @corridor")}
+}
+
+class GoRight(val right : Right? = null, val enter: Enter? = null, val corridor : Corridor? = null): Intent() {
+    override fun getExamples(lang: Language): List<String> {
+        return listOf("@enter @right @corridor", "@enter the @right @corridor", "@enter @corridor @right",
+        "@enter the @corridor @right", "@enter @right", "@enter the @right", "@right", "@right @corridor",
+        "@corridor @right", "the @right @corridor")}
+}
+
